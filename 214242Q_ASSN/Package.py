25f6792fa05f6b439267b091d2bba9726d4f8109
MikuChanUwU <57163606+MikuChanUwU@users.noreply.github.com>
2022-04-18 00:13:36 +0800

    transfer

---
+++ 214242Q_ASSN/Package.py
@@ -0,0 +1,32 @@
+class Package:
+    def __init__(self, packageName, customerName, pax, cost):
+        self.__packageName = packageName
+        self.__customerName = customerName
+        self.__pax = pax
+        self.__cost = cost
+
+    def getPackageName(self):
+        return self.__packageName
+    
+    def getCustomerName(self):
+        return self.__customerName
+
+    def getPax(self):
+        return self.__pax
+    
+    def getCost(self):
+        return self.__cost
+    
+    def setPackageName(self, packageName):
+        self.__packageName = packageName
+    
+    def setCustomerName(self, customerName):
+        self.__customerName = customerName
+
+    def setPax(self, pax):
+        self.__pax = pax
+
+    def setCost(self, cost):
+        self.__cost = cost
+
+     
