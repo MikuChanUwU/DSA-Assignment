62095a330342cca86ee3969ecc257b88d34de3c4
MikuChanUwU <57163606+MikuChanUwU@users.noreply.github.com>
2022-04-18 16:50:48 +0800

    backbone of function 1

--- 214242Q_ASSN/app.py
+++ 214242Q_ASSN/app.py
@@ -5,7 +5,7 @@ while switch == True:
     print(" 1. Display all records \n 2. Sort record by Customer Name using Bubble sort \n 3. Sort record by Package Name using Selection sort \n 4. Sort record by Package Cost using Insertion sort \n 5. Search record by Customer Name using Linear Search and update record \n 6. Search record by Package Name using Binary Search and update record \n 7. List records range from $X to $Y. e.g $100-200 \n 0. Exit Application")
     picker = input("Enter a function number: ")
     if picker == "1":
-        pass
+        print("|   Package Name   |   Customer Name   |   No. of Pax   |   Package Cost per Pax   |")
     elif picker == "2":
         pass
     elif picker == "3":
