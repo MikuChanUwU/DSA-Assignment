9e09243396f9161c6523fc06383e8a77e78358ad
MikuChanUwU <57163606+MikuChanUwU@users.noreply.github.com>
2022-04-18 16:45:05 +0800

    backbone of the loop created

--- 214242Q_ASSN/app.py
+++ 214242Q_ASSN/app.py
@@ -1,1 +1,25 @@
-from .Package import Package
+
+switch = True
+
+while switch == True:
+    print(" 1. Display all records \n 2. Sort record by Customer Name using Bubble sort \n 3. Sort record by Package Name using Selection sort \n 4. Sort record by Package Cost using Insertion sort \n 5. Search record by Customer Name using Linear Search and update record \n 6. Search record by Package Name using Binary Search and update record \n 7. List records range from $X to $Y. e.g $100-200 \n 0. Exit Application")
+    picker = input("Enter a function number: ")
+    if picker == "1":
+        pass
+    elif picker == "2":
+        pass
+    elif picker == "3":
+        pass
+    elif picker == "4":
+        pass
+    elif picker == "5":
+        pass
+    elif picker == "6":
+        pass
+    elif picker == "7":
+        pass
+    elif picker == "0":
+        print("Goodbye.")
+        break
+    else:
+        print("Invalid input, please enter a valid function number.")
